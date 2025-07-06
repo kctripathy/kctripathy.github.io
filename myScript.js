3dfdd18462aa3d0e8f90a2eaf55320e6044de159
Kishor Tripathy <kctripathy@gmail.com>
2025-07-06 17:21:50 +0530

    added json to get data

---
+++ myScript.js
@@ -0,0 +1,137 @@
+function sayHello() {
+  alert("Hello");
+}
+
+function showPage(pageName) {
+  var btns = $("#navbarNav .navbar-nav .nav-link");
+
+  for (var i = 0; i < btns.length; i++) {
+    console.log(btns[i].innerText.toLowerCase());
+    var section_name = btns[i].innerText.toLowerCase();
+    var page_section = document.getElementById(section_name);
+
+    if (btns[i].innerText.toLowerCase() == pageName) {
+      btns[i].classList.add("active-menu");
+    } else {
+      btns[i].classList.remove("active-menu");
+    }
+
+    page_section.className = "d-none";
+  }
+
+  var page_current = document.getElementById(pageName);
+  page_current.className = "d-block";
+
+  if (pageName == "downloads") {
+    fetchDownloads();
+  } else if (pageName == "projects") {
+    fetchProject();
+  }
+
+  return false;
+}
+
+// function fetchDownloads() {
+//   fetch("./assets/data/downloads.json")
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//       }
+//       return response.json();
+//     })
+//     .then((data) => console.log(data))
+//     .catch((error) => console.error("Failed to fetch data:", error));
+// }
+
+function fetchProject() {
+  fetch("./assets/data/projects.json")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("page-title").innerHTML = data.title;
+
+      const container = document.getElementById("project-list");
+      container.innerHTML = "";
+      // data.projects.forEach(project => {
+      //   const div = document.createElement('div');
+      //   div.classList.add('row','project');
+      //   div.innerHTML = `<h3>${project.name}</h3><p>${project.description}</p>`;
+      //   container.appendChild(div);
+      // });
+
+      var ctr = 0;
+      data.projects.forEach((project) => {
+        ctr++;
+        const div = document.createElement("div");
+        div.classList.add("row", "project");
+        div.innerHTML = `<div class='col-12 m-1'> 
+                                <div class='row'>
+                                    <div class='col-9'>#${ctr}: <span class='project-title'>${project.name}</span></div>
+                                    
+                                    <div class='col-3 text-secondary'>${project.duration}</div>
+                                </div>
+                            </div>
+                            <div class='col-12 m-1'>${project.description}</div>
+                            <div class='col-12 m-1'><span class='project-sub-title'>Project for Company: </span>${project.company}</div>
+                            <div class='col-12 m-1'><span class='project-sub-title'>Technologies Used: </span> 
+                                ${getTech(project.technologies)}
+                            </div>
+                            <div class='col-12 m-2'><span class='project-sub-title'>My Role: </span> 
+                                ${project.role}
+                            </div>
+                            `;
+
+        //   div.innerHTML = `<h3>${project.name}</h3><p>${project.description}</p>`;
+        container.appendChild(div);
+      });
+    })
+    .catch((error) => {
+      document.getElementById("page-title").innerHTML =
+        "Error loading projects.";
+      console.error("Error:", error);
+    });
+}
+
+function fetchDownloads() {
+  fetch("./assets/data/downloads.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const container = document.getElementById("download-list");
+      container.innerHTML = "";
+
+      var ctr = 0;
+      data.downloads.forEach((download) => {
+        ctr++;
+        const div = document.createElement("div");
+        div.classList.add("row", "download");
+        div.innerHTML = `<div class='col-3 m-1'> 
+                                 <a href='${download.link}' target='_blank'>${download.name}</a>
+                        </div>`;
+
+        container.appendChild(div);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function fetchJSONData(page) {
+  fetch("./sample.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Failed to fetch data:", error));
+}
+
+function getTech(technologies) {
+    var returnText = "";
+    technologies.forEach((technology) => {
+        returnText = returnText + technology + ", ";
+    });
+
+    return returnText.substring(0, returnText.length - 2);;
+}
